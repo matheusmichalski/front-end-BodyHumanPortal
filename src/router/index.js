9c3579fe342e9d30fc4bbffc1076d213c80efbe2
nicolefemello <nicfmello@gmail.com>
2025-01-07 12:54:30 -0300

    STYLE: new design home

--- src/router/index.js
+++ src/router/index.js
@@ -1,94 +1,112 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import HomeView from '../views/HomeView.vue'
+// GENERAL
+import HomeView from "../views/general/HomeView.vue";
+import ContactView from '@/views/general/ContactView.vue';
 
-// Cadastro
-import CadastroView from '@/views/Cadastro/CadastroView.vue'
-import LoginView from '@/views/Cadastro/LoginView.vue'
+//SYSTEMS
+import BoneView from '@/views/systems/BoneView.vue';
+import CirculatoryView from '@/views/systems/CirculatoryView.vue';
+import DigestiveView from '@/views/systems/DigestiveView.vue';
+import MuscleView from '@/views/systems/MuscleView.vue';
+import NervousView from '@/views/systems/NervousView.vue';
+import ReproductiveFemView from '@/views/systems/ReproductiveFemView.vue';
+import ReproductiveMalView from '@/views/systems/ReproductiveMalView.vue';
+import RespiratoryView from '@/views/systems/RespiratoryView.vue';
+import UrinaryView from '@/views/systems/UrinaryView.vue';
 
-// Calculadora
-import IMCView from '@/views/Calculadora/IMCView.vue'
+//PROFILE
+import DashboardView from '@/views/profile/DashboardView.vue';
+import SegurityView from '@/views/profile/SegurityView.vue';
+import SuportView from '@/views/profile/SuportView.vue';
 
-// Dashboard
-import DashboardView from '@/views/Dashboard/DashboardView.vue'
-import SegurancaView from '@/views/Dashboard/SegurancaView.vue'
-import SuporteView from '@/views/Dashboard/SuporteView.vue'
-
-// Sobre
-import SobreView from '@/views/Sobre/SobreView.vue'
-import ContatoView from '@/views/Sobre/ContatoView.vue'
-import FeedbackView from '@/views/Sobre/FeedbackView.vue'
-import ReferenciasView from '@/views/Sobre/ReferenciasView.vue'
-
-// Sistemas
-import CardiovascularView from '../views/sistemas/CardiovascularView.vue'
-import DigestorioView from '../views/sistemas/DigestorioView.vue'
-import EsqueleticoView from '../views/sistemas/EsqueleticoView.vue'
-import NervosoView from '../views/sistemas/NervosoView.vue'
-import ReprodutorFemininoView from '../views/sistemas/Reprodutor-femininoView.vue'
-import ReprodutorMasculinoView from '../views/sistemas/Reprodutor-masculinoView.vue'
-import RespiratorioView from '../views/sistemas/RespiratorioView.vue'
-import UrinarioView from '../views/sistemas/UrinarioView.vue'
-
-import authToken from '@/middleware/authToken.js'
+//AUTH
+import SigInView from '@/views/auth/SigInView.vue';
+import SigUpView from '@/views/auth/sigUpView.vue';
 
 const router = createRouter({
-  scrollBehavior(to, from, savedPosition) {
-    // always scroll to top
-    return { top: 0 }
-  },
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', name: 'home', component: HomeView },
     {
-      path: '/dashboard',
-      name: 'minhaConta',
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/contato',
+      name: 'contact',
+      component: ContactView
+    },
+    {
+      path: '/sistema/',
+      name: 'systems',
+      children: [
+        {
+          path: 'osseo',
+          component: BoneView
+        },
+        {
+          path: 'cardiovascular',
+          component: CirculatoryView
+        },
+        {
+          path: 'digestivo',
+          component: DigestiveView
+        },
+        {
+          path: 'muscular',
+          component: MuscleView
+        },
+        {
+          path: 'nervoso',
+          component: NervousView
+        },
+        {
+          path: 'reprodutor-feminino',
+          component: ReproductiveFemView
+        },
+        {
+          path: 'reprodutor-masculino',
+          component: ReproductiveMalView
+        },
+        {
+          path: 'respiratorio',
+          component: RespiratoryView
+        },
+        {
+          path: 'urinario',
+          component: UrinaryView
+        }
+      ]
+    },
+    {
+      path: '/minha-conta/',
+      name: 'profile',
       component: DashboardView,
-      beforeEnter: authToken,
-    }, // Protegida
-    {
-      path: '/seguranca',
-      name: 'seguranca',
-      component: SegurancaView,
-      beforeEnter: authToken,
+      children: [
+        {
+          path: 'seguranca',
+          component: SegurityView
+        },
+        {
+          path: 'suporte',
+          component: SuportView
+        }
+      ]
     },
     {
-      path: '/suporte',
-      name: 'suporte',
-      component: SuporteView,
-      beforeEnter: authToken,
-    },
-    { path: '/cadastro', name: 'cadastro', component: CadastroView },
-    { path: '/login', name: 'login', component: LoginView },
-    { path: '/sobre', name: 'sobre', component: SobreView },
-    { path: '/contato', name: 'contato', component: ContatoView },
-    { path: '/feedback', name: 'feedback', component: FeedbackView },
-    {
-      path: '/cardiovascular',
-      name: 'cardiovascular',
-      component: CardiovascularView,
-    },
-    { path: '/digestorio', name: 'digestorio', component: DigestorioView },
-    { path: '/esqueletico', name: 'esqueletico', component: EsqueleticoView },
-    { path: '/nervoso', name: 'nervoso', component: NervosoView },
-    {
-      path: '/reprodutor-feminino',
-      name: 'reprodutor-feminino',
-      component: ReprodutorFemininoView,
-    },
-    {
-      path: '/reprodutor-masculino',
-      name: 'reprodutor-masculino',
-      component: ReprodutorMasculinoView,
-    },
-    {
-      path: '/respiratorio',
-      name: 'respiratorio',
-      component: RespiratorioView,
-    },
-    { path: '/urinario', name: 'urinario', component: UrinarioView },
-    { path: '/referencias', name: 'referencias', component: ReferenciasView },
-    { path: '/imc', name: 'imc', component: IMCView },
+      path: '/auth/',
+      children: [
+        {
+          path: 'login',
+          component: SigInView
+        },
+        {
+          path: 'cadastre-se',
+          component: SigUpView
+        }
+      ]
+    }
   ],
 })
 
