a6723295b284fb8b4be52ab893eeafbdffd4d93b
matheusmichalski <matheus.michalskifc@gmail,com>
2025-01-08 19:06:55 -0300

    RAW: Changed verify arquives names config in eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
 import js from '@eslint/js'
-import pluginVue from 'eslint-plugin-vue'
+import pluginVue, { rules } from 'eslint-plugin-vue'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
 export default [
@@ -7,10 +7,12 @@ export default [
     name: 'app/files-to-lint',
     files: ['**/*.{js,mjs,jsx,vue}'],
   },
-
   {
     name: 'app/files-to-ignore',
     ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+  },
+  {
+    'filenames/match-regex': ['error', '^([a-zA-Z0-9]+)$'],
   },
 
   js.configs.recommended,
--- src/views/general/ContactView.vue
+++ src/views/general/ContactView.vue
@@ -1,15 +1,13 @@
 <script setup>
-import developers from '@/components/desktop/general/contact/developers.vue';
-import ourDescription from '@/components/desktop/general/contact/ourDescription.vue';
+import developers from '@/components/desktop/general/contact/developers.vue'
+import ourDescription from '@/components/desktop/general/contact/ourDescription.vue'
 </script>
 
 <template>
   <main>
     <developers />
     <ourDescription />
-</main>
+  </main>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
