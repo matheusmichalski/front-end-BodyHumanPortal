f0e97145a42754fc983a7d3dbdacc13f07b5e4e5
matheusmichalski <matheus.michalskifc@gmail,com>
2025-01-28 10:30:28 -0300

    FEAT: Init Google Login with vue3-google-login

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import './assets/main.css'
+import GoogleLoginPlugin from 'vue3-google-login'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
 app.use(router)
+app.use(GoogleLoginPlugin, {
+  clientId: import.meta.env.VITE_CLIENT_ID, // Substitua pelo seu Client ID do Google
+})
 
 app.mount('#app')
