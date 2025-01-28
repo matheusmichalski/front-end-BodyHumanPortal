0ac425f18cc7b9eceb62f74afe43128db958106e
rafael.otto <rafael.august.otto.ifc@gmail.com>
2025-01-28 10:01:08 -0300

    FEAT: Componentization and animation of recovery pages and fixes on the email recovery page #03

---
+++ src/components/mobile/recovery/recovery.vue
@@ -0,0 +1,122 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps(['title', 'text', 'button'])
+
+const email = ref('')
+const focused = ref(false)
+
+const checkInput = () => {
+  if (!email.value) {
+    focused.value = false
+  }
+}
+</script>
+
+<template>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Tilt+Neon&family=Tilt+Warp&display=swap" rel="stylesheet">
+
+  <section class="is-mobile">
+    <div class="texts">
+      <img src="../../../../public/logo.png" alt="logo" />
+
+      <h1>{{ title }}</h1>
+
+      <p>{{ text }}</p>
+    </div>
+    <form action="#">
+      <ul>
+        <li><label :class="{ active: focused || email }" for="email">Email</label></li>
+        <li>
+          <input type="email" name="email" id="email" class="email" v-model="email" @focus="focused = true" @blur="checkInput" required
+          />
+        </li>
+      </ul>
+
+      <input type="submit" name="submit" id="submit" value="Confirmar" />
+    </form>
+  </section>
+</template>
+
+<style scoped>
+.is-mobile {
+  display: block;
+  text-align: center;
+  font-size: 5vw;
+  font-family: "Tilt Neon", 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+  font
+
+    .texts {
+      img {
+        height: 20vw;
+        width: auto;
+        margin: 5vw 0 8vw 0;
+      }
+      h1 {
+        color: #1663a3;
+        font-size: 10vw;
+        font-weight: 500;
+        font-family: 'Tilt Warp', 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande',
+        'Lucida Sans', Arial, sans-serif;
+      }
+      p {
+        color: #10293D;
+        margin: 6vw 0 6vw 0;
+      }
+    }
+  form {
+    /*Email field animation */
+
+    ul {
+      list-style: none;
+
+      li {
+        position: relative;
+        margin-bottom: 2vw;
+
+        label {
+          position: absolute;
+          left: 1vw;
+          top: 2.5vw;
+          transform: translateY(-1vw);
+          font-size: 5vw;
+          color: #10293D50;
+          transition: all 0.3s ease-in-out;
+          pointer-events: none;
+          z-index: 1;
+
+          &.active {
+            top: -2vw;
+            font-size: 3vw;
+            color: #10293D;
+            padding: 0 0.5vw;
+          }
+        }
+
+        input {
+          width: 85vw;
+          padding: 1vw;
+          font-size: 5vw;
+          border: none;
+          border-bottom: #10293D solid 0.15vw;
+        }
+      }
+    }
+    /* Button */
+    input[type='submit'] {
+      margin-top: 3vw;
+      width: 55vw;
+      padding: 3vw;
+      font-size: 5vw;
+      background: linear-gradient(to right, #47ABC6, #1F527C);
+      color: white;
+      font-weight: 600;
+      border: none;
+      border-radius: 10vw;
+      cursor: pointer;
+    }
+  }
+}
+</style>
--- src/views/auth/RecoveryEmailView.vue
+++ src/views/auth/RecoveryEmailView.vue
@@ -1,65 +1,13 @@
-<script setup></script>
+<script setup>
+import recovery from '../../components/mobile/recovery/recovery.vue'
+</script>
 
 <template>
-  <section class="is-mobile">
-    <div class="logo">
-      <img src="../../../public/logo.png" alt="Logo" />
-    </div>
-
-    <h1>Email para recuperação</h1>
-
-    <ul>
-      <li>
-        <p>
-          Essa conta está vinculada ao email xyz******@gmail.com. Confirme o seu email no campo
-          abaixo para receber um código de recuperação:
-        </p>
-      </li>
-      <li><img src="#" alt="Email"></li>
-    </ul>
-
-    <form action="#">
-      <div class="emailFild">
-        <label for="Email">Email</label>
-        <input type="email" name="email" id="email" placeholder="Email" />
-      </div>
-      <button>Enviar código</button>
-    </form>
-  </section>
+<main>
+  <recovery
+  title="Email para recuperação"
+  text="Essa conta está vinculada ao email xyz******@gmail.com. Confirme o seu email no campo abaixo para receber um código de recuperação:"
+  
+  />
+</main>
 </template>
-
-<style scoped>
-section.is-mobile {
-  display: block;
-  justify-content: center;
-  text-align: center;
-
-  .logo {
-    margin: 2vw 0 5vw 0;
-
-    img {
-      height: 20vw;
-      width: auto;
-    }
-  }
-  h1 {
-    color: #1663a3;
-    font-size: 2rem;
-    font-weight: 900;
-  }
-  ul li {
-    p {
-      margin: 5vw 0 5vw 0;
-    color: #10293d;
-  }
-  }
-  .emailFild {
-    label {
-      color: #10293d;
-    }
-    input {
-      border-bottom: #10293d;
-    }
-  }
-}
-</style>
