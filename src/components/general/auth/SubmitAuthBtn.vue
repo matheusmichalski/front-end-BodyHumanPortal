1eec449bc1cad50c458f9c77d3bf478fe1ba73e6
matheusmichalski <matheus.michalskifc@gmail,com>
2025-01-28 15:09:50 -0300

    STYLE: Create props to Auth and Install FontAwesomeIcons

---
+++ src/components/general/auth/SubmitAuthBtn.vue
@@ -0,0 +1,24 @@
+<script setup>
+defineProps(['text'])
+</script>
+
+<template>
+  <button type="submit" name="submit" id="submit" value="Confirmar">
+    {{ text }}
+  </button>
+</template>
+
+<style scoped>
+button[type='submit'] {
+  margin-top: 3vw;
+  width: 55vw;
+  padding: 3vw;
+  font-size: 5vw;
+  background: linear-gradient(to right, #47abc6, #1f527c);
+  color: white;
+  font-weight: 600;
+  border: none;
+  border-radius: 10vw;
+  cursor: pointer;
+}
+</style>
--- src/views/auth/RecoveryEmailView.vue
+++ src/views/auth/RecoveryEmailView.vue
@@ -1,13 +1,13 @@
 <script setup>
+import InputAuthBtn from '@/components/general/auth/InputAuthBtn.vue'
 import recovery from '../../components/mobile/recovery/recovery.vue'
 </script>
 
 <template>
-<main>
-  <recovery
-  title="Email para recuperação"
-  text="Essa conta está vinculada ao email xyz******@gmail.com. Confirme o seu email no campo abaixo para receber um código de recuperação:"
-  
-  />
-</main>
+  <main>
+    <recovery
+      title="Email para recuperação"
+      text="Essa conta está vinculada ao email xyz******@gmail.com. Confirme o seu email no campo abaixo para receber um código de recuperação:"
+    />
+  </main>
 </template>
