cdd9cfc9bbc727bc8985cc794a7266f601133786
matheusmichalski <matheus.michalskifc@gmail,com>
2025-01-30 11:47:55 -0300

    FEAT: Delete vue3-google-login and init with script on index.html

--- src/views/auth/SigInView.vue
+++ src/views/auth/SigInView.vue
@@ -3,12 +3,18 @@ import SignInDesktop from '@/components/desktop/auth/SignInDesktop.vue'
 </script>
 
 <template>
+<<<<<<< HEAD
   <main>
     <SignInDesktop />
   </main>
+=======
+  <div class="div">
+    <GoogleLoginBtn />
+  </div>
+>>>>>>> 25e2008 (FEAT: Delete vue3-google-login and init with script on index.html)
 </template>
 
-<style>
+<style scoped>
 div {
   text-align: center;
 }
