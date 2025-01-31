03ab5c73151219f5cb1025b5b145d43d1fa5667c
rafael-otto <rafael.august.otto.ifc@gmail.com>
2025-01-31 15:49:29 -0300

    FEAT: starting the systems page, mobile version

--- src/views/systems/CirculatoryView.vue
+++ src/views/systems/CirculatoryView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import systems from '../../components/desktop/systems/systems.vue'
+import systemsMobile from '../../components/mobile/systems/systemsMobile.vue';
 </script>
 
 <template>
