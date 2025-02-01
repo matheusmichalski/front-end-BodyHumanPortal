61af44bd91354ddc493b356d8e2a7f5f81e8ffb3
matheusmichalski <matheus.michalskifc@gmail,com>
2025-02-01 12:39:38 -0300

    FEAT:Create google Login

--- src/components/desktop/auth/GoogleLoginBtn.vue
+++ src/components/desktop/auth/GoogleLoginBtn.vue
@@ -44,17 +44,6 @@ onMounted(() => {
 function handleCredentialResponse(response) {
   console.log('Encoded JWT ID token: ' + response.credential)
 }
-window.onload = function () {
-  google.accounts.id.initialize({
-    client_id: import.meta.env.VITE_CLIENT_ID,
-    callback: handleCredentialResponse,
-  })
-  google.accounts.id.renderButton(document.getElementById('buttonDiv'), {
-    theme: 'outline',
-    size: 'large',
-  })
-  google.accounts.id.prompt()
-}
 
 onMounted(() => {
   google.accounts.id.initialize({
