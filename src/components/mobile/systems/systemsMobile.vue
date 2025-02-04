072fcdecea149ab160685ff2a627c8c95c431595
rafael-otto <rafael.august.otto.ifc@gmail.com>
2025-02-04 14:29:54 -0300

    STYLE: Formatting the mobile version systems page

--- src/components/mobile/systems/systemsMobile.vue
+++ src/components/mobile/systems/systemsMobile.vue
@@ -26,11 +26,12 @@ defineProps([
 
 <template>
   <section class="is-mobile">
+    <div class="banner">
     <h1>
       Sistema <span>{{ system }}</span>
     </h1>
-    <img src="/systems/squareDetail.png" alt="Detalhe" id="detail1" />
-
+    <img src="/public/systems/Ellipse 14.png" alt="Detalhe" id="detail1" />
+  </div>
     <div class="content">
       <h3>Estrutura</h3>
       <p>{{ content1p }}</p>
@@ -38,7 +39,7 @@ defineProps([
         <a :href="content1Reference"><span>Referência</span></a>
       </p>
     </div>
-    <img src="/systems/bigSquare.png" alt="Detalhe" id="detail2" />
+    <img src="/public/systems/Ellipse 13.png" alt="Detalhe" id="detail2" />
     <div class="content">
       <h3>Cuidados</h3>
       <ul>
@@ -58,7 +59,7 @@ defineProps([
       </div>
     </div>
 
-    <img src="/systems/squareSmall.png" alt="Detalhe" id="detail3" />
+    <img src="/public/systems/Ellipse 15.png" alt="Detalhe" id="detail3" />
 
     <h2>Doenças</h2>
     <div class="illnesses">
@@ -85,7 +86,7 @@ defineProps([
       <p><a :href="content3Reference">Referência</a></p>
     </div>
 
-    <img src="/systems/miniSquare.png" alt="Detalhe" id="detail4" />
+    <img src="/public/systems/Ellipse 16.png" alt="Detalhe" id="detail4" />
 
     <RouterLink to="/" id="button"><button>OUTROS SISTEMAS</button></RouterLink>
   </section>
@@ -93,11 +94,55 @@ defineProps([
 
 <style scoped>
 .is-mobile {
-  background-color: blue;
+
+  .banner {
+    display: flex;
+
+    img {
+      transform: translateX(35vw);
+    }
 
   h1 {
-    color: black;
-    font-size: 3rem   ;
+    padding-left: 2vw;
+    color: #1F527C;
+    font-family: "Tilt Warp";
+    font-size: 1.5rem;
+    display: grid;
+    text-align: center;
+    justify-content: left;
+    margin-top: 5vw;
+
+    span {
+      color: black;
+      margin-bottom: 10vw;
+    }
   }
+  }
+
+  .content {
+    background-color: #A7E6EB;
+    padding:  5vw;
+    border-radius: 24px;
+
+    h3 {
+      font-size: 1.5rem;
+      color: white;
+      font-family: "Tilt Warp";
+    }
+    p {
+      font-family: "Urbanist";
+      color: #474747;
+      margin: 2vw 0 2vw 0;
+    }
+
+    a {
+      color: #103F65;
+      font-weight: 900;
+      font-size: 0.8rem;
+      text-decoration: none;
+    }
+  }
+
 }
+
 </style>
--- src/views/systems/CirculatoryView.vue
+++ src/views/systems/CirculatoryView.vue
@@ -41,6 +41,41 @@ import systemsMobile from '../../components/mobile/systems/systemsMobile.vue'
       content3p="Cuidar do sistema cardiovascular é fundamental para uma vida saudável. Uma dieta equilibrada, atividade física regular e a adoção de hábitos saudáveis contribuem para manter o coração e os vasos sanguíneos em boas condições, prevenindo doenças e estimulando o bem-estar geral."
       content3Reference="ref"
     />
-    <systemsMobile />
+    <systemsMobile
+    system="Cardivascular"
+      content1p="O sistema cardiovascular é composto por:
+    <ul>
+      <li>Coração: Órgão central do sistema</li>
+      <li>Vasos sanguíneos: são tubos por onde o sangue circula.
+        <p>Três principais vasos sanguíneos:  artérias, capilares e veias.</p>
+      </li>
+    </ul>"
+      content1Reference="ref"
+      content2p="<ul>
+      <li>Pratique atividades físicas regularmente;</li>
+      <li>Priorize o cuidado com sua mente;</li>
+      <li>Controle o seu peso de forma adequada;</li>
+      <li>Evite o tabagismo e modere o consumo de álcool;</li>
+      <li>Tenha uma boa qualidade de sono;</li>
+      <li>Mantenha acompanhamento médico regular.</li>
+    </ul>"
+      content2Reference="ref"
+      organImage="/header/circulatory.png"
+      organName="Coração"
+      organh3="Sistema Cardiovascular"
+      organp="O sistema cardiovascular é composto pelo coração e os vasos sanguíneos. Esse sistema tem como função principal realizar a circulação do sangue no corpo, garantindo que oxigênio e nutrientes cheguem às células e que os resíduos metabólicos sejam levados até o local de eliminação. Além disso, o sistema também possibilita, indiretamente, que nosso corpo se defenda contra infecções, já que no sangue estão presentes células e proteínas de defesa."
+      organReference="https://mundoeducacao.uol.com.br/biologia/sistema-circulatorio.htm#:~:text=Resumo%20sobre%20o%20sistema%20cardiovascular&text=O%20cora%C3%A7%C3%A3o%20funciona%20como%20uma%20bomba%2C%20permitindo%20que%20o%20sangue,retorno%20do%20sangue%20ao%20cora%C3%A7%C3%A3o"
+      illness1h3="Insuficiência cardíaca"
+      illness1p="Também conhecida como insuficiência cardíaca congestiva, é uma doença que ocorre quando o coração não bombeia sangue o suficiente para atender às demandas do corpo. Por consequência, o fluxo sanguíneo pode se concentrar nas pernas, pulmões e em diversos outros tecidos do corpo. Os principais sintomas incluem inchaço dos pés e pernas, falta de ar e sensação de cansaço. É uma doença sem cura que pode levar a vários riscos e complicações se não for controlada e tratada de maneira adequada e no momento certo."
+      illness1Reference="https://www.medtronic.com/br-pt/your-health/conditions/heart-failure.html"
+      illness2h3="Hipertensão arterial"
+      illness2p="Também conhecida como pressão alta, é uma doença crônica marcada pelo aumento dos níveis de pressão sanguínea nas artérias. A hipertensão faz com que o coração precise trabalhar mais do que o normal para bombear o sangue de maneira eficiente pelo corpo. Geralmente, a hipertensão não causa sintomas, mas em alguns casos, pode ser percebida por meio de sintomas como tontura, dor de cabeça, alterações na visão e dor no peito. A hipertensão arterial não tem cura, mas pode ser prevenida."
+      illness2Reference="https://www.gov.br/saude/pt-br/assuntos/saude-de-a-a-z/h/hipertensao"
+      illness3h3="Infarto agudo do miocárdio (IAM)"
+      illness3p="Conhecido também como ataque cardíaco, acontece devido à interrupção do fluxo sanguíneo para o coração, na maioria das vezes por causa do acúmulo de gordura nas artérias do coração. Os principais sintomas envolvem dor intensa no peito, essa dor, geralmente, passa para os braços e ombros do lado esquerdo. O infarto do miocárdio não tem 'cura' no sentido de consertar totalmente os danos ao coração. Porém, o tratamento e a ajuda médica podem variar conforme o estágio do infarto e a rapidez com que a pessoa recebe atendimento."
+      illness3Reference="https://www.tuasaude.com/doencas-cardiovasculares/"
+      content3p="Cuidar do sistema cardiovascular é fundamental para uma vida saudável. Uma dieta equilibrada, atividade física regular e a adoção de hábitos saudáveis contribuem para manter o coração e os vasos sanguíneos em boas condições, prevenindo doenças e estimulando o bem-estar geral."
+      content3Reference="ref"
+    />
   </main>
 </template>
