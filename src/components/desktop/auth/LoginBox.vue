65145ae86c15cdce7e592613422ffd1e780d54fc
VitorAndreSIlva <alunovitorandre@gmail.com>
2025-02-04 14:33:07 -0300

    FEAT:start login page

---
+++ src/components/desktop/auth/LoginBox.vue
@@ -0,0 +1,9 @@
+<script setup>
+    import InputAuthBtn from '../../general/InputAuthBtn.vue';
+</script>
+<template>
+    <h1>Login</h1>
+    <InputAuthBtn />
+</template>
+<style scoped>
+</style>
