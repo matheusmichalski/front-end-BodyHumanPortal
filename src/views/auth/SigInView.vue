d8ee8e25c567ea8ad30ef4f77889f09c085ab009
VitorAndreSIlva <alunovitorandre@gmail.com>
2025-02-05 11:18:30 -0300

    FEAT:LoginBox input's

--- src/views/auth/SigInView.vue
+++ src/views/auth/SigInView.vue
@@ -1,4 +1,5 @@
 <script setup>
+<<<<<<< HEAD
 import SignInDesktop from '@/components/desktop/auth/SignInDesktop.vue'
 </script>
 
@@ -6,6 +7,19 @@ import SignInDesktop from '@/components/desktop/auth/SignInDesktop.vue'
   <main>
     <SignInDesktop />
   </main>
+=======
+import GoogleLoginBtn from '@/components/desktop/auth/GoogleLoginBtn.vue'
+import LoginBox from '../../components/desktop/auth/LoginBox.vue';
+</script>
+
+<template>
+  <div>
+    <LoginBox />
+  </div>
+  <div class="div">
+    <GoogleLoginBtn />
+  </div>
+>>>>>>> 48d9148 (FEAT:LoginBox input's)
 </template>
 
 <style></style>
