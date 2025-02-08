d8d27c83411928cb03068de3e5a9ab5950f3735e
matheusmichalski <matheus.michalskifc@gmail,com>
2025-02-08 15:32:56 -0300

    FEAT: Replace SignIn and SignUp components with desktop versions and update Google Login button styling

--- src/components/desktop/auth/GoogleLoginBtn.vue
+++ src/components/desktop/auth/GoogleLoginBtn.vue
@@ -30,7 +30,6 @@ onMounted(() => {
   display: flex;
   justify-content: center;
   align-items: center;
-  transform: scale(1.4);
 }
 </style>
   <GoogleLogin @success="onSuccess" @error="onError">
