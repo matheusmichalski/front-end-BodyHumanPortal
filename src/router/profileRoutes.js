cdc6a2303f1ba42fb43d945028231552df72b106
matheusmichalski <matheus.michalskifc@gmail,com>
2025-02-08 15:59:51 -0300

    FEAT: Add profile routes and improve security page structure

--- src/router/profileRoutes.js
+++ src/router/profileRoutes.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import DashboardView from '@/views/profile/DashboardView.vue'
 import SegurityView from '@/views/profile/SegurityView.vue'
 import SuportView from '@/views/profile/SuportView.vue'
@@ -6,6 +7,16 @@ import MyProfileLayout from '@/layouts/MyProfileLayout.vue'
 const profileRoutes = {
   path: '/minha-conta',
   component: MyProfileLayout,
+=======
+import MainLayout from '@/layouts/MainLayout.vue'
+import DashboardView from '@/views/profile/DashboardView.vue'
+import SegurityView from '@/views/profile/SegurityView.vue'
+import SuportView from '@/views/profile/SuportView.vue'
+
+const profileRoutes = {
+  path: '/minha-conta',
+  component: MainLayout,
+>>>>>>> 456f27d (FEAT: Add profile routes and improve security page structure)
   children: [
     {
       path: '',
@@ -13,12 +24,20 @@ const profileRoutes = {
       component: DashboardView,
     },
     {
+<<<<<<< HEAD
       path: '/seguranca',
+=======
+      path: 'seguranca',
+>>>>>>> 456f27d (FEAT: Add profile routes and improve security page structure)
       name: 'segurity',
       component: SegurityView,
     },
     {
+<<<<<<< HEAD
       path: '/suporte',
+=======
+      path: 'suporte',
+>>>>>>> 456f27d (FEAT: Add profile routes and improve security page structure)
       name: 'suport',
       component: SuportView,
     },
