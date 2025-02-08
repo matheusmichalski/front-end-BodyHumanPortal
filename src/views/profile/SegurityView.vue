826a68d33e46fe41228fc0e9215977615525f168
rafael-otto <rafael.august.otto.ifc@gmail.com>
2025-02-08 15:28:30 -0300

    FEAT: Starting the security page (mobile version)

--- src/views/profile/SegurityView.vue
+++ src/views/profile/SegurityView.vue
@@ -1,10 +1,13 @@
 <script setup>
-import HeaderView from '@/components/desktop/elements/HeaderView.vue'
 import segurity from '@/components/desktop/my-profile/segurity.vue'
+import securityMobile from '@/components/mobile/my-profile/securityMobile.vue';
 </script>
 
 <template>
-  <main>
+  <main class="is-desktop">
     <segurity />
+  </main>
+  <main class="is-mobile">
+    <securityMobile />
   </main>
 </template>
