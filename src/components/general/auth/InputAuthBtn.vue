0cfc649ff14a63b4ffb5a861f65637ddb7b7ea81
nicolefemello <nicfmello@gmail.com>
2025-02-10 12:59:23 -0300

    FEAT: Add logo to recovery components and adjust layout for improved aesthetics

--- src/components/general/auth/InputAuthBtn.vue
+++ src/components/general/auth/InputAuthBtn.vue
@@ -70,10 +70,6 @@ ul {
       border-bottom: #10293d solid 0.15vw;
       outline: none;
       background-color: transparent;
-      outline: none;
-       outline: none;
-
-      outline: none;
     }
   }
 }
