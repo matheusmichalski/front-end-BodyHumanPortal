7a68c04fce28926847bda8dc31b689688c1427d4
nicolefemello <nicfmello@gmail.com>
2025-02-10 17:25:34 -0300

    FEAT: Implement carousel functionality for illness cards on mobile systems page

--- src/components/mobile/systems/systemsMobile.vue
+++ src/components/mobile/systems/systemsMobile.vue
@@ -24,33 +24,32 @@
     'content3Reference', //content3
   ])
 
-  const indexStart = ref(0);
-  function getCardStartStyle(cardIndex) {
-    const position = (cardIndex - indexStart.value + 3) % 3
+const indexStart = ref(0);
+function getCardStartStyle(cardIndex) {
+  const position = (cardIndex - indexStart.value + 3) % 3
 
-    const styles = [
-      {
-        transform: "translateX(-80vw)",
-        opacity: 0
-      },
-      {
-        transform: 'translateX(0vw) scale(1)',
-        opacity: 1,
-      },
-      {
-        transform: "translateX(75vw)",
-        opacity: 0
-      }
-    ]
+  const styles = [
+    {
+      transform: 'translateX(-10vw)', // Primeiro
+      opacity: 0.5,
+    },
+    {
+      transform: 'translateX(10vw)', // Segundo
+      opacity: 1,
+    },
+    {
+      opacity: 0,
+    }
+  ]
 
-    return styles[position] || styles[0] // Garante o estilo mesmo para índices fora do alcance
-  }
-  function nextStart() {
-    indexStart.value = (indexStart.value + 1) % 3
-  }
-  function prevStart() {
-    indexStart.value = (indexStart.value - 1 + 3) % 3
-  }
+  return styles[position] || styles[0] // Garante o estilo mesmo para índices fora do alcance
+}
+function nextStart() {
+  indexStart.value = (indexStart.value == 3) ? 0 : indexStart.value + 1;
+}
+function prevStart() {
+  indexStart.value = (indexStart.value == 0) ? 2 : indexStart.value - 1;
+}
 </script>
 
 <template>
@@ -122,6 +121,26 @@
             </div>
           </div>
       </div>
+      <div id="container">
+        <i class="fa-solid fa-chevron-left button-start" id="prev-start" @click="prevStart"></i>
+        <i class="fa-solid fa-chevron-right button-start" id="next-start" @click="nextStart"></i>
+        <div class="illness" :style="getCardStartStyle(1)">
+          <h3>{{ illness1h3 }}</h3>
+          <p>{{ illness1p }}</p>
+          <p><a :href="illness1Reference">Referência</a></p>
+        </div>
+        <div class="illness" :style="getCardStartStyle(2)">
+          <h3>{{ illness2h3 }}</h3>
+          <p>{{ illness2p }}</p>
+          <p><a :href="illness2Reference">Referência</a></p>
+        </div>
+        <div class="illness" :style="getCardStartStyle(3)">
+          <h3>{{ illness3h3 }}</h3>
+          <p>{{ illness3p }}</p>
+          <p><a :href="illness3Reference">Referência</a></p>
+        </div>
+      </div>
+    </div>
 
       <div class="content">
         <h3>Conclusão</h3>
@@ -146,11 +165,10 @@
       padding-left: 10vw;
       padding-top: 5vw;
       font-family: "Tilt Warp";
-      font-size: 10vw;
+      font-size: 2rem;
       display: grid;
       justify-content: left;
       margin-top: 5vw;
-      text-align: left;
       color: #1F527C;
 
       span {
@@ -218,7 +236,7 @@
       a {
         color: #103F65;
         font-weight: 900;
-        font-size: 4vw;
+        font-size: 0.8rem;
         text-decoration: none;
       }
     }
@@ -404,5 +422,70 @@
   }
 }
 
+  .illnesses {
+    margin: 4vw 0 5vw 0;
+    padding: 3vw 0 5vw 0;
+    height: 25vw;
+    position: relative;
+    overflow: hidden;
+
+    #container {
+      display: flex;
+      justify-content: center;
+      overflow: hidden;
+
+      .button-start {
+        font-size: 2vw;
+        color: #006DBC;
+        position: absolute;
+        cursor: pointer;
+        z-index: 2;
+        top: 50%;
+        transform: translateY(-50%);
+      }
+
+      #prev-start {
+        left: 3vw;
+      }
+
+      #next-start {
+        right: 3vw;
+      }
+
+      .illness {
+        width: 35.719vw;
+        border-radius: 1.5vw;
+        border: 1px solid #103F65;
+        padding: 2vw 1.5vw 2vw 1.5vw;
+        margin: 0 2vw 0 2vw;
+        transition: transform 0.5s ease;
+      }
+    }
+  }
+
+  /*.illnesses {
+
+    #container {
+      display: flex;
+      justify-content: center;
+      overflow: hidden;
+      margin: 2vw 0 5vw 2vw;
+
+      .illness {
+        width: 35.719vw;
+        border-radius: 1.5vw;
+        border: 1px solid #103F65;
+        padding: 2vw 1.5vw 2vw 1.5vw;
+        margin: 0 2vw 0 2vw;
+
+        h2 {
+          font-size: 1.8rem;
+          color: #103F65;
+          font-family: "Tilt Warp";
+          margin-bottom: 2vw;
+        }
+      }
+    }
+  }*/
 }
 </style>
