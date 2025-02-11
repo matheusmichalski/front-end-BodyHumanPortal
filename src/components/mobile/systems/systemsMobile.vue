5445497e9525d962474d43f88de7d6e5350d6c97
nicolefemello <nicfmello@gmail.com>
2025-02-11 14:11:39 -0300

    FEAT: Enhance Nervous and Bone views with detailed system information and health tips

--- src/components/mobile/systems/systemsMobile.vue
+++ src/components/mobile/systems/systemsMobile.vue
@@ -24,32 +24,33 @@
     'content3Reference', //content3
   ])
 
-const indexStart = ref(0);
-function getCardStartStyle(cardIndex) {
-  const position = (cardIndex - indexStart.value + 3) % 3
+  const indexStart = ref(0);
+  function getCardStartStyle(cardIndex) {
+    const position = (cardIndex - indexStart.value + 3) % 3
 
-  const styles = [
-    {
-      transform: 'translateX(-10vw)', // Primeiro
-      opacity: 0.5,
-    },
-    {
-      transform: 'translateX(10vw)', // Segundo
-      opacity: 1,
-    },
-    {
-      opacity: 0,
-    }
-  ]
+    const styles = [
+      {
+        transform: "translateX(-80vw)",
+        opacity: 0
+      },
+      {
+        transform: 'translateX(0vw) scale(1)',
+        opacity: 1,
+      },
+      {
+        transform: "translateX(75vw)",
+        opacity: 0
+      }
+    ]
 
-  return styles[position] || styles[0] // Garante o estilo mesmo para índices fora do alcance
-}
-function nextStart() {
-  indexStart.value = (indexStart.value == 3) ? 0 : indexStart.value + 1;
-}
-function prevStart() {
-  indexStart.value = (indexStart.value == 0) ? 2 : indexStart.value - 1;
-}
+    return styles[position] || styles[0] // Garante o estilo mesmo para índices fora do alcance
+  }
+  function nextStart() {
+    indexStart.value = (indexStart.value + 1) % 3
+  }
+  function prevStart() {
+    indexStart.value = (indexStart.value - 1 + 3) % 3
+  }
 </script>
 
 <template>
@@ -62,7 +63,7 @@ function prevStart() {
 
     <div class="content">
       <h3>Estrutura</h3>
-      <p>{{ content1p }}</p>
+      <p v-html="content1p"></p>
       <p>
         <a :href="content1Reference"><span>Referência</span></a>
       </p>
@@ -70,10 +71,7 @@ function prevStart() {
 
     <div class="content">
       <h3>Cuidados</h3>
-      <ul>
-        <li v-for="care of content2p" :key="care.id">{{ care.content }}</li>
-      </ul>
-      <p>{{ content2p }}</p>
+      <p v-html="content2p"></p>
       <p><a :href="content2Reference">Referência</a></p>
     </div>
 
@@ -82,8 +80,8 @@ function prevStart() {
         <img :src="organImage" :alt="organName" />
       </div>
       <div class="text">
-        <h3>{{ organh3 }}</h3>
-        <p>{{ organp }}</p>
+        <h3></h3>
+        <p></p>
         <p><a :href="organReference">Referência</a></p>
       </div>
     </div>
@@ -116,7 +114,7 @@ function prevStart() {
 
     <div class="content">
       <h3>Conclusão</h3>
-      <p>{{ content3p }}</p>
+      <p v-html="content3p"></p>
       <p><a :href="content3Reference">Referência</a></p>
     </div>
   </section>
@@ -125,7 +123,7 @@ function prevStart() {
 <style scoped>
 .is-mobile {
   padding: 0;
-  background: url(/public/systems/Fundo.png) no-repeat top center;
+  background: url(/systems/Fundo.png) no-repeat top center;
   background-size: 100%;
 
   .banner {
@@ -135,10 +133,11 @@ function prevStart() {
       padding-left: 10vw;
       padding-top: 5vw;
       font-family: "Tilt Warp";
-      font-size: 2rem;
+      font-size: 10vw;
       display: grid;
       justify-content: left;
       margin-top: 5vw;
+      text-align: left;
       color: #1F527C;
 
       span {
@@ -153,15 +152,18 @@ function prevStart() {
     padding: 5vw;
     border-radius: 24px;
     margin: 0 4vw 6vw 4vw;
+    text-align: left;
 
     h3 {
-      font-size: 1.5rem;
-      color: white;
+      font-size: 7vw;
+      color: #10293D;
       font-family: "Tilt Warp";
     }
 
     p {
       font-family: "Urbanist";
+      font-size: 5vw;
+      line-height: 5.5vw;
       color: #474747;
       margin: 2vw 0 2vw 0;
     }
@@ -169,7 +171,7 @@ function prevStart() {
     a {
       color: #103F65;
       font-weight: 900;
-      font-size: 0.8rem;
+      font-size: 4vw;
       text-decoration: none;
     }
   }
@@ -180,7 +182,7 @@ function prevStart() {
     }
 
     h3 {
-      font-size: 1.5rem;
+      font-size: 7vw;
       color: #103F65;
       font-family: "Tilt Warp";
       margin-bottom: 2vw;
@@ -190,6 +192,8 @@ function prevStart() {
       font-family: "Urbanist";
       color: #474747;
       margin-bottom: 2vw;
+      font-size: 5vw;
+      line-height: 5.5vw;
     }
 
     .text {
@@ -201,32 +205,50 @@ function prevStart() {
       a {
         color: #103F65;
         font-weight: 900;
-        font-size: 0.8rem;
+        font-size: 4vw;
         text-decoration: none;
       }
     }
   }
 
   .illnesses {
-    margin: 4vw 0 5vw 0;
-    padding: 3vw 0 5vw 0;
-    height: 25vw;
+    margin-top: 5vw;
+    padding-top: 3vw;
     position: relative;
     overflow: hidden;
+
+    h2 {
+      font-family: 'Tilt Warp';
+      font-weight: 400;
+      font-size: 7vw;
+      color: #103f65;
+      text-align: left;
+      margin-left: 5vw;
+    }
 
     #container {
       display: flex;
       justify-content: center;
-      overflow: hidden;
+      position: relative;
+      width: 100%;
+      height: 180vw;
+      margin-top: 5vw;
 
       .button-start {
-        font-size: 2vw;
-        color: #006DBC;
+        width: 5.823vw;
+        height: 5.823vw;
+        border-radius: 50%;
+        background-color: #4A899A;
+        font-size: 2.5vw;
+        color: #010A5C;
         position: absolute;
         cursor: pointer;
         z-index: 2;
-        top: 50%;
-        transform: translateY(-50%);
+        top: 30%;
+        transform: translateY(250%);
+        display: flex;
+        align-items: center;
+        justify-content: center;
       }
 
       #prev-start {
@@ -238,39 +260,44 @@ function prevStart() {
       }
 
       .illness {
-        width: 35.719vw;
+        width: 70vw;
+        height: 90%;
         border-radius: 1.5vw;
         border: 1px solid #103F65;
-        padding: 2vw 1.5vw 2vw 1.5vw;
-        margin: 0 2vw 0 2vw;
-        transition: transform 0.5s ease;
-      }
-    }
-  }
+        padding: 5vw;
+        margin: 0vw 2vw 0 2vw;
+        position: absolute;
+        transition: transform 0.5s ease-in-out, opacity 0.5s ease-in-out;
 
-  /*.illnesses {
+        h3 {
+          font-family: 'Tilt Warp';
+          font-weight: 400;
+          font-size: 6vw;
+          line-height: 6.5vw;
+          color: #103f65;
+          text-align: left;
+          margin: 1vw 0;
+        }
 
-    #container {
-      display: flex;
-      justify-content: center;
-      overflow: hidden;
-      margin: 2vw 0 5vw 2vw;
+        p {
+          font-family: 'Urbanist';
+          font-weight: 500;
+          font-size: 5vw;
+          line-height: 5.5vw;
+          color: #474747;
+          text-align: left;
+          margin-top: 3vw;
 
-      .illness {
-        width: 35.719vw;
-        border-radius: 1.5vw;
-        border: 1px solid #103F65;
-        padding: 2vw 1.5vw 2vw 1.5vw;
-        margin: 0 2vw 0 2vw;
-
-        h2 {
-          font-size: 1.8rem;
-          color: #103F65;
-          font-family: "Tilt Warp";
-          margin-bottom: 2vw;
+          a {
+            text-decoration: none;
+            font-family: 'Tilt Warp';
+            font-weight: 400;
+            font-size: 4vw;
+            color: #103f65;
+          }
         }
       }
     }
-  }*/
+  }
 }
 </style>
