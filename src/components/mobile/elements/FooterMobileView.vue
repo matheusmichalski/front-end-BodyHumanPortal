7e8a551027bf1c33f267889c7594ce6688e7ddc3
nicolefemello <nicfmello@gmail.com>
2025-02-12 13:37:30 -0300

    FEAT: add mobile components for DashboardView and MainLayout, including mobile footer and layout adjustments

---
+++ src/components/mobile/elements/FooterMobileView.vue
@@ -0,0 +1,50 @@
+<template>
+  <footer class="is-mobile">
+    <hr>
+    <p>&copy; 2024 Todos os direitos reservados - Portal do Corpo Humano</p>
+    <nav>
+      <ul>
+        <li><a href="https://www.instagram.com/portal_corpohumano/"><span class="fa-brands fa-instagram"></span></a>
+        </li>
+        <li><a href="https://mail.google.com/mail/?view=cm&fs=1&to=portalcorpohumano@gmail.com" target="_blank"><span
+              class="fa-regular fa-envelope"></span></a></li>
+      </ul>
+    </nav>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  margin-top: -10vw;
+  padding-bottom: 20vw;
+
+  hr {
+    border: 0.001vw solid #034F85;
+    margin: 3vw 9vw 8vw 8vw;
+    text-align: center;
+  }
+
+  p {
+    font-family: 'Inter';
+    font-size: clamp(0.3rem, 4vw, 1.5rem);
+    padding: 0 8vw;
+    text-align: center;
+  }
+
+  nav {
+    ul {
+      display: flex;
+      justify-content: center;
+
+      li {
+        padding: 3vw 0 5vw 3vw;
+
+        a {
+          color: #103F65;
+          font-size: clamp(0.5rem, 5vw, 2rem);
+        }
+      }
+    }
+  }
+}
+</style>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,6 +2,7 @@
 import HeaderView from '@/components/desktop/elements/HeaderView.vue'
 import HeaderMobileView from '@/components/mobile/elements/HeaderMobileView.vue';
 import FooterView from '@/components/desktop/elements/FooterView.vue'
+import FooterMobileView from '@/components/mobile/elements/FooterMobileView.vue';
 </script>
 
 <template>
@@ -9,4 +10,5 @@ import FooterView from '@/components/desktop/elements/FooterView.vue'
   <HeaderMobileView />
   <RouterView />
   <FooterView />
+  <FooterMobileView />
 </template>
