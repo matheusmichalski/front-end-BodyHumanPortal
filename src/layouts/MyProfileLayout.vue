eac25b78b38579e31cf42cc863f24858c549e3b3
nicolefemello <nicfmello@gmail.com>
2025-02-12 13:37:30 -0300

    FEAT: add mobile components for DashboardView and MainLayout, including mobile footer and layout adjustments

--- src/layouts/MyProfileLayout.vue
+++ src/layouts/MyProfileLayout.vue
@@ -1,5 +1,7 @@
 <script setup>
 import HeaderView from '@/components/desktop/elements/HeaderView.vue'
+import HeaderMobileView from '@/components/mobile/elements/HeaderMobileView.vue';
+import FooterMobileView from '@/components/mobile/elements/FooterMobileView.vue';
 </script>
 <template>
   <div class="is-desktop">
@@ -14,14 +16,10 @@ import HeaderView from '@/components/desktop/elements/HeaderView.vue'
             >
           </li>
           <li>
-            <RouterLink to="/minha-conta/suporte"
-              ><i class="fa-regular fa-circle-question"></i> Suporte</RouterLink
-            >
+            <RouterLink to="/minha-conta/suporte"><i class="fa-regular fa-circle-question"></i> Suporte</RouterLink>
           </li>
           <li>
-            <RouterLink to="/minha-conta/seguranca"
-              ><i class="fa-solid fa-shield-halved"></i> Segurança</RouterLink
-            >
+            <RouterLink to="/minha-conta/seguranca"><i class="fa-solid fa-shield-halved"></i> Segurança</RouterLink>
           </li>
         </ul>
       </nav>
@@ -30,54 +28,65 @@ import HeaderView from '@/components/desktop/elements/HeaderView.vue'
       </div>
     </section>
   </div>
+  <div class="is-mobile">
+    <HeaderMobileView />
+    <RouterView />
+    <FooterMobileView />
+  </div>
 </template>
 
 <style scoped>
-section {
-  margin-top: -3vw;
-  margin-left: -5vw;
-  display: flex;
-  background-color: #a7e6eb;
+.is-desktop {
+  section {
+    margin-top: -3vw;
+    margin-left: -5vw;
+    display: flex;
+    background-color: #a7e6eb;
 
-  #routerContainer {
-    width: 100%;
-    border-radius: 24px;
-    background-color: #ffffff;
-  }
-
-  #sidebar {
-    width: 20.938vw;
-    text-align: center;
-    margin: 0vw 0 0 0vw;
-
-    h1 {
-      font-family: 'Tilt Warp';
-      font-weight: 400;
-      font-size: 1.823vw;
-      line-height: 2.292vw;
-      color: #10293d;
-      margin: 1vw 0 1vw 0;
+    #routerContainer {
+      width: 100%;
+      border-radius: 24px;
+      background-color: #ffffff;
     }
 
-    ul {
-      li {
-        width: 12.812vw;
-        margin: 0.5vw 4vw 0.5vw 4vw;
-        text-align: left;
-        padding: 1vw 0 1vw 0;
+    #sidebar {
+      width: 20.938vw;
+      text-align: center;
+      margin: 0vw 0 0 0vw;
 
-        a {
-          font-family: 'Urbanist';
-          font-weight: 400;
-          font-size: 1.563vw;
-          line-height: 1.875vw;
-          color: #10293d;
-          padding: 0.5vw 0 0.5vw 0;
-          text-decoration: none;
-          border-bottom: 1px solid #10293d;
+      h1 {
+        font-family: 'Tilt Warp';
+        font-weight: 400;
+        font-size: 1.823vw;
+        line-height: 2.292vw;
+        color: #10293d;
+        margin: 1vw 0 1vw 0;
+      }
+
+      ul {
+        li {
+          width: 12.812vw;
+          margin: 0.5vw 4vw 0.5vw 4vw;
+          text-align: left;
+          padding: 1vw 0 1vw 0;
+
+          a {
+            font-family: 'Urbanist';
+            font-weight: 400;
+            font-size: 1.563vw;
+            line-height: 1.875vw;
+            color: #10293d;
+            padding: 0.5vw 0 0.5vw 0;
+            text-decoration: none;
+            border-bottom: 1px solid #10293d;
+          }
         }
       }
     }
   }
+}
+
+.is-mobile {
+  margin-bottom: 0;
 }
 </style>
--- src/views/profile/DashboardView.vue
+++ src/views/profile/DashboardView.vue
@@ -1,9 +1,13 @@
 <script setup>
 import DashboardDesktop from '@/components/desktop/my-profile/dashboardDesktop.vue'
+import dashboardMobile from '@/components/mobile/my-profile/dashboardMobile.vue';
 </script>
 
 <template>
-  <main>
+  <main class="is-desktop">
     <DashboardDesktop />
+  </main>
+  <main class="is-mobile">
+    <dashboardMobile />
   </main>
 </template>
