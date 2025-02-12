232038a966d9ab28da378b896caa82a6115c31b1
nicolefemello <nicfmello@gmail.com>
2025-02-12 11:41:25 -0300

    FEAT: add mobile header component for improved navigation

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,10 +1,12 @@
 <script setup>
 import HeaderView from '@/components/desktop/elements/HeaderView.vue'
+import HeaderMobileView from '@/components/mobile/elements/HeaderMobileView.vue';
 import FooterView from '@/components/desktop/elements/FooterView.vue'
 </script>
 
 <template>
   <HeaderView />
+  <HeaderMobileView />
   <RouterView />
   <FooterView />
 </template>
