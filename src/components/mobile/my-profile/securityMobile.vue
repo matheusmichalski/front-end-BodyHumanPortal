0bb05b19231f315d50b1c474ec12e0e8516f2a1e
nicolefemello <nicfmello@gmail.com>
2025-02-12 13:37:30 -0300

    FEAT: add mobile components for DashboardView and MainLayout, including mobile footer and layout adjustments

--- src/components/mobile/my-profile/securityMobile.vue
+++ src/components/mobile/my-profile/securityMobile.vue
@@ -78,10 +78,6 @@ const deletePassword = ref('')
       text="Excluir conta"
       />
       </div>
-
-
-
-
   </section>
 </template>
 
@@ -95,6 +91,7 @@ const deletePassword = ref('')
     color: #10293d;
     margin-bottom: 5vw;
   }
+
   .info{
     background-color: #EDF8F9;
     padding: 5vw;
@@ -106,6 +103,7 @@ const deletePassword = ref('')
       font-size: max(5vw, 20px);
       color: #10293d;
     }
+
     p {
       margin-top: 2vw;
       font-family: "Urbanist";
@@ -125,11 +123,11 @@ const deletePassword = ref('')
       padding-bottom: 1.2vw;
       border-bottom: #10293d solid 0.15vw;
     }
+
     .Mode {
       width: 76vw;
     }
   }
-
 
 .button {
   text-align: center;
